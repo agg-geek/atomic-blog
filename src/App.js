d72de27b9307983994bbd12d8d11e5b9a56005cd
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-31 18:05:05 +0530

    Fix prop drilling for Header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 import { faker } from '@faker-js/faker';
 
 function createRandomPost() {
@@ -7,6 +7,10 @@ function createRandomPost() {
 		body: faker.hacker.phrase(),
 	};
 }
+
+// PostContext is now a Component
+// PostContext because this will provide values related to the blog's Post
+const PostContext = createContext();
 
 function App() {
 	const [posts, setPosts] = useState(() =>
@@ -40,43 +44,55 @@ function App() {
 	);
 
 	return (
-		<section>
-			<button
-				onClick={() => setIsFakeDark(isFakeDark => !isFakeDark)}
-				className="btn-fake-dark-mode"
-			>
-				{isFakeDark ? '☀️' : '🌙'}
-			</button>
+		// Notice the component tree after creating the Provider
+		<PostContext.Provider
+			value={{
+				posts: searchedPosts,
+				onAddPost: handleAddPost,
+				onClearPosts: handleClearPosts,
+				// we also provide the Query context to PostContext
+				// though we should've created a QueryContext for it
+				searchQuery,
+				setSearchQuery,
+			}}
+		>
+			<section>
+				<button
+					onClick={() => setIsFakeDark(isFakeDark => !isFakeDark)}
+					className="btn-fake-dark-mode"
+				>
+					{isFakeDark ? '☀️' : '🌙'}
+				</button>
 
-			<Header
-				posts={searchedPosts}
-				onClearPosts={handleClearPosts}
-				searchQuery={searchQuery}
-				setSearchQuery={setSearchQuery}
-			/>
-			<Main posts={searchedPosts} onAddPost={handleAddPost} />
-			<Archive onAddPost={handleAddPost} />
-			<Footer />
-		</section>
+				<Header />
+				<Main posts={searchedPosts} onAddPost={handleAddPost} />
+				<Archive onAddPost={handleAddPost} />
+				<Footer />
+			</section>
+		</PostContext.Provider>
 	);
 }
 
-function Header({ posts, onClearPosts, searchQuery, setSearchQuery }) {
+function Header() {
+	const { onClearPosts } = useContext(PostContext.Consumer);
+
 	return (
 		<header>
 			<h1>
 				<span>⚛️</span>The Atomic Blog
 			</h1>
 			<div>
-				<Results posts={posts} />
-				<SearchPosts searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+				<Results />
+				<SearchPosts />
 				<button onClick={onClearPosts}>Clear posts</button>
 			</div>
 		</header>
 	);
 }
 
-function SearchPosts({ searchQuery, setSearchQuery }) {
+function SearchPosts() {
+	const { searchQuery, setSearchQuery } = useContext(PostContext.Consumer);
+
 	return (
 		<input
 			value={searchQuery}
@@ -86,7 +102,8 @@ function SearchPosts({ searchQuery, setSearchQuery }) {
 	);
 }
 
-function Results({ posts }) {
+function Results() {
+	const { posts } = useContext(PostContext.Consumer);
 	return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
